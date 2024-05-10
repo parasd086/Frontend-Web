0ff968a7587db40851df9ab81b06d0861004e0bc
parasd086 <inboxparas1@gmail.com>
2024-05-10 23:53:33 +0530

    ReactJs: 4-movie-geek First Commit

---
+++ ReactJs/4-movie-geek/src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+// import StarRating from "./StarRating";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+    {/* <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    /> */}
+  </React.StrictMode>
+);
