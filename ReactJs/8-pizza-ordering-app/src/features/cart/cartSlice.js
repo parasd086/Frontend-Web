1b604ce2e2ba20bd2943cc2f25b67898461573a4
parasd086 <parasd086@gmail.com>
2024-06-07 23:45:43 +0530

    ReactJs: Add 8-pizza-ordering-app

---
+++ ReactJs/8-pizza-ordering-app/src/features/cart/cartSlice.js
@@ -0,0 +1,71 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [],
+  // cart: [
+  //   {
+  //     pizzaId: 12,
+  //     name: "Medditerranean",
+  //     unitPrice: 16,
+  //     quantity: 2,
+  //     totalPrice: 32,
+  //   },
+  // ],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      //payload = newItem
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      //payload = pizzaId
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQuantity(state, action) {
+      //payload = pizzaId
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    decreaseItemQuantity(state, action) {
+      //payload = pizzaId
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+
+      item.quantity--;
+      item.totalPrice = item.quantity * item.unitPrice;
+
+      if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
+
+// 1. This is now a Redux selector function. And the recommendation is that these functions start with the get keyword and that we have them all in the central place in the cart slice file because we will actually need this operation later on in another component.
+
+//2. Writing selectors like these will have performance issues in larger application. So checkout "reselect" library.
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCart = (state) => state.cart.cart;
+
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
