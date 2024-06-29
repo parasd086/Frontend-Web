33a569004c8ef978295e83fa7835d5a6c65c3ee8
parasd086 <parasd086@gmail.com>
2024-06-29 14:33:49 +0530

    TS: Add 1-maps

--- ReactJs/8-pizza-ordering-app/src/App.jsx
+++ ReactJs/8-pizza-ordering-app/src/App.jsx
@@ -6,6 +6,7 @@ import CreateOrder, {
   action as createOrderAction,
 } from "./features/order/CreateOrder";
 import Order, { loader as orderLoader } from "./features/order/Order";
+import { action as UpdateOrderAction } from "./features/order/UpdateOrder";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
 
@@ -36,6 +37,7 @@ const router = createBrowserRouter([
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: UpdateOrderAction,
       },
     ],
   },
---
+++ Typescript/1-Maps/src/index.ts
@@ -0,0 +1,11 @@
+/// <reference types="@types/google.maps" />
+import { User } from "./User";
+import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
+
+const map = new CustomMap("map");
+const user = new User();
+const company = new Company();
+
+map.addMarker(user);
+map.addMarker(company);
